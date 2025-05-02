ffbc98eb509c7eb3679782f9ae0730df482fd9c2
Aymeric <aymeric.simao@gmail.com>
2025-05-02 17:33:06 +0200

    update routes for trends and create get /trends

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require("./models/connection");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var kicksRouter = require("./routes/kicks");
+var trendsRouter = require("./routes/trends")
 
 var app = express();
 
@@ -25,5 +26,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/kicks", kicksRouter);
+app.use("/trends", trendsRouter)
 
 module.exports = app;
